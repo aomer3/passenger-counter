cc4dbde0db33fbb66be5a4715854f87894021f88
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-10 21:35:51 -0400

    Add Increment and Save button

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let incrementBtn = document.getElementById("increment-btn");
+let saveBtn = document.getElementById("save-btn");
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+saveEl.textContent = "Previous entries: "
+document.body.append(saveEl);
+
+let count = 0;
+
+function increment() {
+    count++;
+    countEl.textContent = count;
+};
+
+function save(){
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    count = 0;
+    countEl.textContent = 0;
+}
+
+
+incrementBtn.addEventListener("click", increment);
+saveBtn.addEventListener("click", save);
+
+
